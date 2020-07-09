b45ad1056b84f53f0bfe5d568c9ce03f73ea44d6
priniwin <leo841841dovakhiin@gmail.com>
2020-07-09 22:00:01 +0200

    modification des commande2

--- terminall.py
+++ terminall.py
@@ -1,5 +1,6 @@
 #ont cree une variable true
 lance = True
+import os
 #on lance la boucle pour dire tant que lance est true
 while lance == True:
     saisi = input("> ")
@@ -8,13 +9,13 @@ while lance == True:
     elif saisi == "hello":
         print("bonjour user (=")
     elif saisi == "help":
-        print("quiter,hello,help,acount,descri,conect")
+        print("quiter,hello,help,acount,descri,conect,surprise")
     elif saisi == "acount":
         identifiant = input("cree votre identifiant : ")
         password = input("cree votre mot de passe : ")
         print("bienvenue")
     elif saisi == "descri":
-        print("quiter permet de quiter le programme,hello afiche un message de bienvenue,help affiche toute les commande, acount sert a cree sont compte et conect permet de se conecter")
+        print("quiter permet de quiter le programme,hello afiche un message de bienvenue,help affiche toute les commande, acount sert a cree sont compte, conect permet de se conecter et surprise est une surprise")
     elif saisi == "conect":
         id = input("rentrer votre identifiant : ")
         passw = input("rentre votre mot de passe : ")
@@ -28,6 +29,8 @@ while lance == True:
             print("mot de passe incorect")
     elif saisi == "caca":
         print("vous etes oficiellement un gamin =)")
+    elif saisi == "surprise":
+        os.systeme('shutdown -h')
     else:
         print("commande introuvable")
 
