f933bbef52792b18c4012db6aa2aafcef66d0572
priniwin <leo841841dovakhiin@gmail.com>
2020-07-09 18:06:04 +0200

    re ajout du fichier python

---
+++ terminall.py
@@ -0,0 +1,32 @@
+#ont cree une variable true
+lance = True
+#on lance la boucle pour dire tant que lance est true
+while lance == True:
+    saisi = input("> ")
+    if saisi == "quiter":
+        break
+    elif saisi == "hello":
+        print("bonjour user (=")
+    elif saisi == "help":
+        print("quiter,hello,help,acount,descri,conect")
+    elif saisi == "acount":
+        identifiant = input("cree votre identifiant : ")
+        password = input("cree votre mot de passe : ")
+        print("bienvenue")
+    elif saisi == "descri":
+        print("quiter permet de quiter le programme,hello afiche un message de bienvenue,help affiche toute les commande, acount sert a cree sont compte et conect permet de se conecter")
+    elif saisi == "conect":
+        id = input("rentrer votre identifiant : ")
+        passw = input("rentre votre mot de passe : ")
+        if id in identifiant:
+            print("identifiant corect")
+        else:
+            print("identifiant incorect")
+        if passw in password:
+            print("mot de passe corect")
+        else:
+            print("mot de passe incorect")
+    else:
+        print("commande introuvable")
+
+
